daa4ff24bf8d305aa750092c9a923225a11bc650
Joshua Kao <joshuakao1904@gmail.com>
2017-08-30 09:43:40 -0700

    hopefully this addresses heroku disconnect

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,36 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var db_config = mysql.createConnection({
   port: 3306,   
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b780b2f71cbafc",
   password: "b18ec129",
   database: "heroku_5284d4c914ea5c8",
-  connectionLimit: 5
 });
 
-// connect
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection;
 
-module.exports = connection;
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = db_config;
